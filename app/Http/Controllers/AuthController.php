03c96b1ddd1497afd0069a5e818e8d315bec767b
Wojciech Przybyła <contact@wpww.pl>
2024-05-12 16:45:46 +0200

    🔐 authentication scaffolding created

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $rq) {
+
+    }
+
+    public function logout(Request $rq) {
+
+    }
+
+    public function register(Request $rq) {
+
+    }
+
+    public function resetPassword(Request $rq) {
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::controller(AuthController::class)->prefix("auth")->group(function () {
+        foreach ([
+            "login",
+            "register",
+            "reset-password",
+        ] as $endpoint) {
+            Route::withoutMiddleware("auth:sanctum")->post($endpoint, Str::camel($endpoint));
+        }
+
+        Route::get('user', fn (Request $request) => $request->user());
+        Route::post("logout", "logout");
+    });
 });
