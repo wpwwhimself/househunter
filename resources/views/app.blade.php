7f7fa8d56166610f31f2e0affca4a1d9da614310
Wojciech Przybyła <contact@wpww.pl>
2024-06-10 23:38:18 +0200

    🔨 built to test api

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-critters-container>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" href="{{ asset('assets/h2color.svg') }}" type="image/svg+xml">
         <title>Househunter</title>
-    </head>
+    <style>:root{--acc-hue: 166;--solids-sat: 3%;--acc: var(--acc-hue), 87%, 49%;--gray: var(--acc-hue), var(--solids-sat), 50%;--fg: var(--acc-hue), var(--solids-sat), 95%;--bg: var(--acc-hue), var(--solids-sat), 12%;--gap-size: 1em}body{background-color:hsl(var(--bg));color:hsl(var(--fg));margin:0;min-height:100vh}*{font-family:Overpass Mono,monospace}*{transition-duration:.3s}
+</style><link rel="stylesheet" href="styles.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles.css"></noscript></head>
     <body>
         <app-root></app-root>
         <script src="{{ asset('polyfills.js') }}" type="module"></script>
         <script src="{{ asset('main.js') }}" type="module"></script>
-    </body>
+    <script src="polyfills.js" type="module"></script><script src="main.js" type="module"></script></body>
 </html>
