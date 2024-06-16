f367c434b6fc4f540dff0bd9048fe08be994b7be
Wojciech Przybyła <contact@wpww.pl>
2024-06-16 14:06:40 +0200

    🔨 cleaned up build files

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ cd resources/angular
 call ng build
 
 :::: copy new files ::::
-xcopy /y dist\app.blade.php ..\views\app.blade.php
+xcopy /y dist\app.template.blade.php ..\views\app.blade.php
 xcopy /y dist\*.js ..\..\public
 xcopy /y dist\*.css ..\..\public
 xcopy /s/y dist\assets\* ..\..\public\assets
---
+++ resources/views/app.template.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-critters-container>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="{{ asset('assets/h2color.svg') }}" type="image/svg+xml">
+        <title>Househunter</title>
+    </head>
+    <body>
+        <app-root></app-root>
+        <script src="{{ asset('polyfills.js') }}" type="module"></script>
+        <script src="{{ asset('main.js') }}" type="module"></script>
+    <script src="polyfills.js" type="module"></script><script src="main.js" type="module"></script><script src="polyfills.js" type="module"></script><script src="main.js" type="module"></script><script src="polyfills.js" type="module"></script><script src="main.js" type="module"></script><script src="polyfills.js" type="module"></script><script src="main.js" type="module"></script><script src="polyfills.js" type="module"></script><script src="main.js" type="module"></script><script src="polyfills.js" type="module"></script><script src="main.js" type="module"></script><script src="polyfills.js" type="module"></script><script src="main.js" type="module"></script><script src="polyfills.js" type="module"></script><script src="main.js" type="module"></script><script src="polyfills.js" type="module"></script><script src="main.js" type="module"></script><script src="polyfills.js" type="module"></script><script src="main.js" type="module"></script><script src="polyfills.js" type="module"></script><script src="main.js" type="module"></script><script src="polyfills.js" type="module"></script><script src="main.js" type="module"></script><script src="polyfills.js" type="module"></script><script src="main.js" type="module"></script></body>
+</html>
